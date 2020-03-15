3ee02e47ada58a1e33e481d6b1e56e2dc3796613
be5invis <belleve@typeof.net>
2020-03-15 04:57:36 -0700

    Add beachball config

---
+++ beachball.config.js
@@ -0,0 +1,11 @@
+/* eslint-env es6,node */
+
+module.exports = {
+    bumpDeps: true,
+    groups: [
+        {
+            name: "ot-builder",
+            include: ["packages/*"]
+        }
+    ]
+};
