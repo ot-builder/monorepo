00fcb843c57d423b7d7c224b15320181305c924e
be5invis <belleve@typeof.net>
2020-03-18 00:33:42 -0700

    Use scan planes to do zone dicing.

--- packages/cli-proc/src/procs/merge-consolidate/consolidate/zone-dicing.test.ts
+++ packages/cli-proc/src/procs/merge-consolidate/consolidate/zone-dicing.test.ts
@@ -1,4 +1,93 @@
-import { diceZones, Span, SrcZone } from "./zone-dicing";
+import { diceZones, Span, SrcZone, DstZone, Zone } from "./zone-dicing";
+
+test("Multi condition set dicing", () => {
+    MultiDiceTest<number>(
+        12,
+        MakeZone(1, [1, 2], [1, 2], [1, 2]),
+        MakeZone(2, [1, 2], [1, 2], [2, 3]),
+        MakeZone(3, [3, 4], [0, 3], [0, 2]),
+        MakeZone(4, [0, 2], [0, 4], [0, 4]),
+        MakeZone(5, [2, 4], [0, 4], [1, 4])
+    );
+
+    MultiDiceTest<number>(
+        2,
+        MakeZone(1, [0, 2], [0, 4], [0, 4]),
+        MakeZone(2, [2, 4], [0, 4], [1, 4])
+    );
+
+    MultiDiceTest<number>(
+        1,
+        MakeZone(1, [0, 2], [0, 4], [0, 4]),
+        MakeZone(2, [0, 2], [0, 4], [0, 4])
+    );
+
+    MultiDiceTest<number>(
+        7,
+        MakeZone(1, [1, 3], [1, 3], [1, 3]),
+        MakeZone(2, [2, 4], [2, 4], [2, 4])
+    );
+});
+
+function MultiDiceTest<S>(zoneCount: number, ...zoneSets: SrcZone<S>[]) {
+    const results = Array.from(diceZones(3, zoneSets));
+    expect(results.length).toBe(zoneCount);
+    validateZoneCoverage(3, zoneSets, results);
+    validateZonesNotOverlapping(3, results);
+    for (const [zone, sSet] of results) validateMultiZone(zoneSets, zone, sSet);
+}
+
+function validateZoneCoverage<S>(dn: number, zoneSets: SrcZone<S>[], results: DstZone<S>[]) {
+    for (const [zone] of zoneSets) {
+        for (const tp of testPointInZone(dn, 0, [], zone)) {
+            let found = false;
+            for (const [dstZone] of results) {
+                let match = true;
+                for (let d = 0; d < dn; d++)
+                    if (tp[d] < dstZone[d][0] || tp[d] > dstZone[d][1]) match = false;
+                if (match) {
+                    found = true;
+                    break;
+                }
+            }
+            expect(found).toBeTruthy();
+        }
+    }
+}
+
+function validateZonesNotOverlapping<S>(dn: number, results: DstZone<S>[]) {
+    for (const [srcZone] of results) {
+        for (const [dstZone] of results) {
+            if (dstZone === srcZone) continue;
+            let overlap = true;
+            for (let d = 0; d < dn; d++) {
+                const overlapMin = Math.min(srcZone[d][0], dstZone[d][0]);
+                const overlapMax = Math.max(srcZone[d][1], dstZone[d][1]);
+                const srcLength = srcZone[d][1] - srcZone[d][0];
+                const dstLength = dstZone[d][1] - dstZone[d][0];
+                if (overlapMax - overlapMin >= srcLength + dstLength) overlap = false;
+            }
+            if (overlap) fail("Zones overlap");
+        }
+    }
+}
+
+function* testPointInZone(
+    dn: number,
+    d: number,
+    carry: number[],
+    zone: Zone
+): IterableIterator<number[]> {
+    if (d >= dn) {
+        yield carry;
+        return;
+    }
+    for (const p of [0, 1 / 2, 1]) {
+        const t = zone[d][0] + p * (zone[d][1] - zone[d][0]);
+        carry[d] = t;
+        yield* testPointInZone(dn, d + 1, carry, zone);
+    }
+}
 
 function validateMultiZone<S>(zoneSets: SrcZone<S>[], zone: Span[], sSet: S[]) {
     const matchingSSet: S[] = [];
@@ -15,90 +104,6 @@ function validateMultiZone<S>(zoneSets: SrcZone<S>[], zone: Span[], sSet: S[]) {
     expect(sSet).toEqual(matchingSSet);
 }
 
-function MultiDiceTest<S>(zoneSets: SrcZone<S>[]) {
-    const results = Array.from(diceZones(3, zoneSets));
-    for (const [zone, sSet] of results) validateMultiZone(zoneSets, zone, sSet);
+function MakeZone<S>(x: S, ...spans: Span[]): SrcZone<S> {
+    return [spans, x];
 }
-
-test("Multi condition set dicing", () => {
-    MultiDiceTest<number>([
-        [
-            [
-                [0, 2],
-                [0, 4],
-                [0, 4]
-            ],
-            4
-        ],
-        [
-            [
-                [2, 4],
-                [0, 4],
-                [1, 4]
-            ],
-            5
-        ],
-        [
-            [
-                [1, 2],
-                [1, 2],
-                [1, 2]
-            ],
-            1
-        ],
-        [
-            [
-                [1, 2],
-                [1, 2],
-                [2, 3]
-            ],
-            2
-        ],
-        [
-            [
-                [3, 4],
-                [0, 3],
-                [0, 2]
-            ],
-            3
-        ]
-    ]);
-
-    MultiDiceTest<number>([
-        [
-            [
-                [0, 2],
-                [0, 4],
-                [0, 4]
-            ],
-            1
-        ],
-        [
-            [
-                [2, 4],
-                [0, 4],
-                [1, 4]
-            ],
-            2
-        ]
-    ]);
-
-    MultiDiceTest<number>([
-        [
-            [
-                [0, 2],
-                [0, 4],
-                [0, 4]
-            ],
-            1
-        ],
-        [
-            [
-                [0, 2],
-                [0, 4],
-                [0, 4]
-            ],
-            2
-        ]
-    ]);
-});
--- packages/cli-proc/src/procs/merge-consolidate/consolidate/zone-dicing.ts
+++ packages/cli-proc/src/procs/merge-consolidate/consolidate/zone-dicing.ts
@@ -5,36 +5,60 @@ export type DstZone<S> = [Zone, S[]];
 
 export function* diceZones<S>(dn: number, zoneSets: SrcZone<S>[]): IterableIterator<DstZone<S>> {
     if (!zoneSets.length) return;
+    yield* scanPlaneDiceZones(dn, 0, [], zoneSets);
+}
 
-    let dicingResults: Map<string, Zone> = new Map();
-    dicingResults.set(hashZone(zoneSets[0][0]), zoneSets[0][0]);
-
-    for (let z = 1; z < zoneSets.length; z++) {
-        const updatedResults: Map<string, Zone> = new Map();
-        for (const oldZone of dicingResults.values()) {
-            for (const newZone of diceTwoZones(dn, oldZone, zoneSets[z][0])) {
-                const zone = copyZone(newZone);
-                const zh = hashZone(zone);
-                updatedResults.set(zh, zone);
-            }
-        }
-        dicingResults = updatedResults;
+function* scanPlaneDiceZones<S>(
+    dn: number,
+    d: number,
+    carry: Zone,
+    zones: SrcZone<S>[]
+): IterableIterator<DstZone<S>> {
+    if (d >= dn) {
+        yield fetchMatchingSs(carry, zones);
+        return;
     }
 
-    for (const zone of dicingResults.values()) {
-        const matchingSSet: S[] = [];
-        for (const [srcZone, s] of zoneSets) {
-            let inZone = true;
-            for (let d = 0; d < srcZone.length; d += 1) {
-                if (zone[d][0] < srcZone[d][0] || zone[d][1] > srcZone[d][1]) {
-                    inZone = false;
-                    break;
-                }
+    const stops = new Set<number>();
+    for (const [zone] of zones) {
+        let shouldInclude = true;
+        for (let dt = 0; dt < d; dt++) {
+            const spanZone = zone[dt];
+            const spanCarry = carry[dt];
+            if (spanCarry[0] < spanZone[0] || spanCarry[1] > spanZone[1]) {
+                shouldInclude = false;
+                break;
             }
-            if (inZone) matchingSSet.push(s);
         }
-        yield [zone, matchingSSet];
+        if (shouldInclude) stops.add(zone[d][0]), stops.add(zone[d][1]);
     }
+
+    const sortedStops = Array.from(stops).sort(asc);
+
+    for (let s = 1; s < sortedStops.length; s++) {
+        carry[d] = [sortedStops[s - 1], sortedStops[s]];
+        yield* scanPlaneDiceZones(dn, d + 1, carry, zones);
+    }
+}
+
+function fetchMatchingSs<S>(carry: Zone, zones: SrcZone<S>[]): DstZone<S> {
+    const matchingSSet: S[] = [];
+    for (const [srcZone, s] of zones) {
+        let inZone = true;
+        for (let d = 0; d < srcZone.length; d += 1) {
+            if (carry[d][0] < srcZone[d][0] || carry[d][1] > srcZone[d][1]) {
+                inZone = false;
+                break;
+            }
+        }
+        if (inZone) matchingSSet.push(s);
+    }
+
+    return [carry.slice(0), matchingSSet];
+}
+
+function asc(a: number, b: number) {
+    return a - b;
 }
 
 export function hashZone(zone: Zone) {
@@ -43,95 +67,4 @@ export function hashZone(zone: Zone) {
         result += `${zone[d][0]};${zone[d][1]}/`;
     }
     return result;
-}
-
-function copyZone(x: Zone) {
-    const result: Zone = [];
-    for (let d = 0; d < x.length; d++) result[d] = [x[d][0], x[d][1]];
-    return result;
-}
-
-function* diceTwoZones<D>(dimCount: number, condSet1: Zone, condSet2: Zone) {
-    if (dimCount <= 0) return;
-    const telescope: Zone = [];
-    for (let d = 0; d < dimCount; d++) telescope[d] = [0, 0];
-    yield* diceTwoZonesImpl(dimCount, 0, 3, telescope, condSet1, condSet2);
-}
-
-function* diceTwoZonesImpl(
-    dn: number,
-    d: number,
-    carry: number,
-    result: Zone,
-    zone1: Zone,
-    zone2: Zone
-): IterableIterator<Zone> {
-    if (!carry) return;
-    if (d >= dn) {
-        yield result;
-        return;
-    }
-    if (carry === 1) {
-        yield* stopDicing(dn, d, result, zone1);
-        return;
-    }
-    if (carry === 2) {
-        yield* stopDicing(dn, d, result, zone2);
-        return;
-    }
-
-    const span1 = zone1[d];
-    const span2 = zone2[d];
-    for (const mask of dimMask(d, result, span1, span2))
-        yield* diceTwoZonesImpl(dn, d + 1, carry & mask, result, zone1, zone2);
-}
-
-function* stopDicing(dn: number, d: number, result: Zone, zone: Zone): IterableIterator<Zone> {
-    for (let dd = d; dd < dn; dd++) {
-        result[dd][0] = zone[dd][0];
-        result[dd][1] = zone[dd][1];
-    }
-    yield result;
-}
-
-function* dimMask(d: number, result: Zone, span1: Span, span2: Span) {
-    if (span1[0] <= span2[0]) {
-        yield* dimMaskImpl(d, result, span1, span2, 1, 2);
-    } else {
-        yield* dimMaskImpl(d, result, span2, span1, 2, 1);
-    }
-}
-
-function* dimMaskImpl(
-    d: number,
-    result: Zone,
-    span1: Span,
-    span2: Span,
-    one: number,
-    two: number
-) {
-    if (span1[1] <= span2[0]) {
-        if (withLen(d, result, span1[0], span1[1])) yield one;
-        if (withLen(d, result, span2[0], span2[1])) yield two;
-    } else if (span1[1] <= span2[1]) {
-        // [  {  ]  }
-        if (withLen(d, result, span1[0], span2[0])) yield one;
-        if (withLen(d, result, span2[0], span1[1])) yield one | two;
-        if (withLen(d, result, span1[1], span2[1])) yield two;
-    } else {
-        // [  {  }  ]
-        if (withLen(d, result, span1[0], span2[0])) yield one;
-        if (withLen(d, result, span2[0], span2[1])) yield one | two;
-        if (withLen(d, result, span2[1], span1[1])) yield one;
-    }
-}
-
-function withLen(d: number, result: Zone, min: number, max: number) {
-    if (max > min) {
-        result[d][0] = min;
-        result[d][1] = max;
-        return true;
-    } else {
-        return false;
-    }
 }
