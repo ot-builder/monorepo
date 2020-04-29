6b60751887761fa2486f69e3aed90169aad43ebe
Belleve Invis <belleve@typeof.net>
2020-04-29 03:54:02 -0700

    TTC Bundle: fix arg handler (#46)
    * Fix argument handler of `-o`
    
    * Change files
    
    * Throw error when no input/output is specified.

--- packages/cli-ttc-bundle/src/arg-parser.ts
+++ packages/cli-ttc-bundle/src/arg-parser.ts
@@ -10,15 +10,27 @@ export class ArgParser {
 
     arg(x: string) {
         if (x[0] === "-" && this.acceptArgs) {
-            if (x === "--") this.acceptArgs = false;
-            else if (x === "-h" || x === "--help") this.displayHelp = true;
-            else if (x === "-u" || x === "--unify") this.unify = true;
-            else if (x === "-x" || x === "--sparse") this.sparse = true;
-            else if (x === "-o") this.argName = x;
-            else throw new Error("Unrecognized option " + x);
+            this.handleOption(x);
         } else {
+            this.handleArgument(x);
+        }
+    }
+
+    private handleOption(x: string) {
+        if (x === "--") this.acceptArgs = false;
+        else if (x === "-h" || x === "--help") this.displayHelp = true;
+        else if (x === "-u" || x === "--unify") this.unify = true;
+        else if (x === "-x" || x === "--sparse") this.sparse = true;
+        else if (x === "-o") this.argName = x;
+        else throw new Error("Unrecognized option " + x);
+    }
+
+    private handleArgument(x: string) {
+        if (this.argName) {
             if (this.argName === "-o") this.output = x;
-            else this.inputs.push(x);
+            this.argName = null;
+        } else {
+            this.inputs.push(x);
         }
     }
 }
