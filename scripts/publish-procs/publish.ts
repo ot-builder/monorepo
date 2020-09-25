195577e78521f0ecc4a536c76726d9bc5e31ed83
otbbuilder-dev <otbbuilder-dev@users.noreply.github.com>
2020-09-25 02:09:23 -0700

    Fixup auth

--- scripts/publish-procs/publish.ts
+++ scripts/publish-procs/publish.ts
@@ -6,11 +6,9 @@ export async function publish(cfg: PublishConfig) {
     }
 
     // Setup Git
-    const repoUrl = `https://${cfg.GitUser}:${cfg.GitToken}@github.com/ot-builder/monorepo.git`;
 
     await Git("config", "user.name", cfg.GitUser);
     await Git("config", "user.email", cfg.GitEmail);
-    await Git("remote", "set-url", "origin", repoUrl);
 
     // Do the publish
     await Beachball("publish", "--yes", "-n", cfg.NpmToken);
