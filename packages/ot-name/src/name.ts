a560b0c1ee4ea23805fb5f14e051bdb2299b43e0
be5invis <belleve@typeof.net>
2020-12-06 20:09:34 -0800

    Export NameID enumeration

--- packages/ot-name/src/name.ts
+++ packages/ot-name/src/name.ts
@@ -13,4 +13,31 @@ export namespace Name {
         public records: Record[] = [];
         public langTagMap: Data.Maybe<string[]> = null;
     }
+    export enum NameID {
+        Copyright = 0,
+        LegacyFamily = 1,
+        LegacySubfamily = 2,
+        UniqueFontId = 3,
+        FullFontName = 4,
+        VersionString = 5,
+        PostscriptName = 6,
+        Trademark = 7,
+        Manufacturer = 8,
+        Designer = 9,
+        Description = 10,
+        UrlVendor = 11,
+        UrlDesigner = 12,
+        LicenseDescription = 13,
+        LicenseInfoUrl = 14,
+        PreferredFamily = 16,
+        PreferredSubfamily = 17,
+        CompatibleFull = 18,
+        SampleText = 19,
+        PostscriptCidFindfont = 20,
+        WwsFamily = 21,
+        WwsSubfamily = 22,
+        LightBackgroundPalette = 23,
+        DarkBackgroundPalette = 24,
+        VariationsPostScriptNamePrefix = 25
+    }
 }
