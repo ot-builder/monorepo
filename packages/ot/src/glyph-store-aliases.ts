d542d28ca2ab0db8873cf4426fc325cb4924ae8f
Belleve <belleve@typeof.net>
2021-01-14 18:35:05 -0800

    Finalize V1 interfaces (#84)
    * Finalize V1 interface changes
    
    * Change files

---
+++ packages/ot/src/glyph-store-aliases.ts
@@ -0,0 +1,12 @@
+import { ImpLib } from "@ot-builder/common-impl";
+import { OtGlyph } from "@ot-builder/ot-glyphs";
+import { Data } from "@ot-builder/prelude";
+
+export type GlyphOrder = Data.Order<OtGlyph>;
+export type ListGlyphStore = ImpLib.Order.ListStore<OtGlyph>;
+export type GlyphStore = Data.OrderStore<OtGlyph>;
+export type GlyphStoreFactory<GS extends GlyphStore> = Data.OrderStoreFactory<OtGlyph, GS>;
+export type GlyphStoreFactoryWithDefault<
+    GS extends GlyphStore
+> = Data.OrderStoreFactoryWithDefault<OtGlyph, GS>;
+export { OtListGlyphStoreFactory as ListGlyphStoreFactory } from "@ot-builder/ot-glyphs";
--- packages/ot/src/index.ts
+++ packages/ot/src/index.ts
@@ -1,54 +1,15 @@
-import { ImpLib } from "@ot-builder/common-impl";
-import { OtEncoding } from "@ot-builder/ot-encoding";
-import { OtExtPrivate } from "@ot-builder/ot-ext-private";
-import { CffCoGlyphs, OtGlyph, TtfCoGlyphs } from "@ot-builder/ot-glyphs";
-import { OtFontLayoutData } from "@ot-builder/ot-layout";
-import { OtFontMetadata } from "@ot-builder/ot-metadata";
-import { OtNameData } from "@ot-builder/ot-name";
-import { Data } from "@ot-builder/prelude";
-
-export type Font<GS extends Data.OrderStore<OtGlyph> = Data.OrderStore<OtGlyph>> =
-    | Font.Cff<GS>
-    | Font.Ttf<GS>;
-export namespace Font {
-    // TypeDefs
-    type OtFontShared = OtFontMetadata & OtEncoding & OtFontLayoutData & OtNameData & OtExtPrivate;
-    export type Cff<
-        GS extends Data.OrderStore<OtGlyph> = Data.OrderStore<OtGlyph>
-    > = OtFontShared & CffCoGlyphs & { glyphs: GS };
-    export type Ttf<
-        GS extends Data.OrderStore<OtGlyph> = Data.OrderStore<OtGlyph>
-    > = OtFontShared & TtfCoGlyphs & { glyphs: GS };
-
-    export function isCff<GS extends Data.OrderStore<OtGlyph>>(font: Font<GS>): font is Cff<GS> {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return !!(font as any).cff;
-    }
-    export function isTtf<GS extends Data.OrderStore<OtGlyph>>(font: Font<GS>): font is Ttf<GS> {
-        return !isCff(font);
-    }
-}
-
+export * from "./font";
 export { Sfnt } from "@ot-builder/ot-sfnt";
 
-export { OtVar as Var, GeneralVar } from "@ot-builder/variance";
+export { OtVar as Var } from "@ot-builder/variance";
 
 export { Head, Maxp, Os2, MetricHead, Fvar, Post, Avar, Gasp } from "@ot-builder/ot-metadata";
 
-export { OtGlyph as Glyph, GeneralGlyph } from "@ot-builder/ot-glyphs";
-export { Cff, Cvt, Fpgm, Prep } from "@ot-builder/ot-glyphs";
+export { OtGlyph as Glyph, Cff, Cvt, Fpgm, Prep } from "@ot-builder/ot-glyphs";
 export { OtGlyphNamingSource as GlyphNamingSource } from "@ot-builder/ot-glyphs";
 export { OtGlyphNamer as GlyphNamer } from "@ot-builder/ot-glyphs";
 export { OtStandardGlyphNamer as StandardGlyphNamer } from "@ot-builder/ot-standard-glyph-namer";
-
-export type GlyphOrder = Data.Order<OtGlyph>;
-export type ListGlyphStore = ImpLib.Order.ListStore<OtGlyph>;
-export type GlyphStore = Data.OrderStore<OtGlyph>;
-export type GlyphStoreFactory<GS extends GlyphStore> = Data.OrderStoreFactory<OtGlyph, GS>;
-export type GlyphStoreFactoryWithDefault<
-    GS extends GlyphStore
-> = Data.OrderStoreFactoryWithDefault<OtGlyph, GS>;
-export { OtListGlyphStoreFactory as ListGlyphStoreFactory } from "@ot-builder/ot-glyphs";
+export * from "./glyph-store-aliases";
 
 export { Cmap, OtEncoding as Encoding } from "@ot-builder/ot-encoding";
 export { Name, Stat, Meta } from "@ot-builder/ot-name";
