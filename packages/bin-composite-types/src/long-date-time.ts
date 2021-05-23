75f547a3b8d992e369bc2bd647d0ddd93aea290c
Cyano Hao <c@cyano.cn>
2021-05-23 07:57:52 +0700

    Simplify epoch calculation

--- packages/bin-composite-types/src/long-date-time.ts
+++ packages/bin-composite-types/src/long-date-time.ts
@@ -1,16 +1,16 @@
 import { Read, Write } from "@ot-builder/bin-util";
 
 const Power2To32 = 0x0100000000;
-const EpochDifference = Date.UTC(1970, 1, 1) - Date.UTC(1904, 1, 1);
+const OtBaseEpoch = Date.UTC(1904, 1, 1);
 
 export const LongDateTime = {
     ...Read<Date>(view => {
         const high = view.uint32();
         const low = view.uint32();
-        return new Date((high * Power2To32 + low) * 1000 - EpochDifference);
+        return new Date((high * Power2To32 + low) * 1000 + OtBaseEpoch);
     }),
     ...Write<Date>((fr, date) => {
-        const time = Math.round((date.getTime() + EpochDifference) / 1000);
+        const time = Math.round((date.getTime() - OtBaseEpoch) / 1000);
         const high = (time / Power2To32) >>> 0;
         const low = time % Power2To32;
         fr.uint32(high);
