e25394f33847b471e54eb2400bc00493136c4b87
otbbuilder-dev <otbbuilder-dev@users.noreply.github.com>
2021-09-18 00:35:37 -0700

    Improve rectification performance by removing unnecessary currying when rectify geometries

--- packages/rectify/src/glyph/index.ts
+++ packages/rectify/src/glyph/index.ts
@@ -9,95 +9,94 @@ import {
 } from "../interface";
 import { RectifyImpl } from "../shared";
 
-interface PointAttachmentGlobalState {
-    processed: Set<Ot.Glyph>;
+interface GlyphRectifyGlobalState {
+    processedGlyphs: Set<Ot.Glyph>;
 }
-interface PointAttachmentHandlerState {
+interface GlyphRectifyHandlerState {
     points: Data.XY<Ot.Var.Value>[];
 }
-type PointAttachmentHandler = (st: PointAttachmentHandlerState) => null | Ot.Glyph.Geometry;
 
-export class OtGhRectifyGeomPointAttachmentAlg {
+class GeometryProcessor {
     constructor(
         private readonly recGlyphRef: GlyphReferenceRectifier,
         private readonly recCoord: CoordRectifier,
         private readonly recPA: PointAttachmentRectifier,
-        private readonly context: PointAttachmentGlobalState
+        private readonly globalState: GlyphRectifyGlobalState
     ) {}
 
-    public process(geom: Ot.Glyph.Geometry): PointAttachmentHandler {
+    public process(
+        geom: Ot.Glyph.Geometry,
+        st: GlyphRectifyHandlerState
+    ): null | Ot.Glyph.Geometry {
         switch (geom.type) {
             case Ot.Glyph.GeometryType.ContourSet:
-                return this.contourSet(geom);
+                return this.processContourSet(geom, st);
             case Ot.Glyph.GeometryType.GeometryList:
-                return this.geometryList(geom.items.map(item => this.process(item)));
+                return this.processGeometryList(geom.items, st);
             case Ot.Glyph.GeometryType.TtReference:
-                return this.ttReference(geom);
+                return this.processTtReference(geom, st);
         }
     }
 
-    public contourSet(cs: Ot.Glyph.ContourSetProps) {
-        return (st: PointAttachmentHandlerState) => {
-            const cs1: Ot.Glyph.Contour[] = [];
-            for (const c of cs.contours) {
-                const c1: Ot.Glyph.Contour = [];
-                for (let zid = 0; zid < c.length; zid++) {
-                    const z = c[zid];
-                    c1[zid] = Ot.Glyph.Point.create(
-                        this.recCoord.coord(z.x),
-                        this.recCoord.coord(z.y),
-                        z.kind
-                    );
-                }
-                cs1.push(c1);
-            }
-
-            const g = new Ot.Glyph.ContourSet(cs1);
-            for (const c of g.contours) for (const z of c) st.points.push(z);
-            return g;
-        };
-    }
-    public geometryList(processes: PointAttachmentHandler[]) {
-        return (st: PointAttachmentHandlerState) => {
-            const children: Ot.Glyph.Geometry[] = [];
-            for (const proc of processes) {
-                const childGeom = proc(st);
-                if (childGeom) children.push(childGeom);
-            }
-            return new Ot.Glyph.GeometryList(children);
-        };
-    }
-    public ttReference(ref: Ot.Glyph.TtReferenceProps) {
-        return (st: PointAttachmentHandlerState) => {
-            const to1 = this.recGlyphRef.glyphRef(ref.to);
-            if (!to1) return null;
-
-            processGlyph(this.recGlyphRef, this.recCoord, this.recPA, to1, this.context);
-            const ref1 = new Ot.Glyph.TtReference(to1, {
-                ...ref.transform,
-                dx: this.recCoord.coord(ref.transform.dx),
-                dy: this.recCoord.coord(ref.transform.dy)
-            });
-            ref1.roundXyToGrid = ref.roundXyToGrid;
-            ref1.useMyMetrics = ref.useMyMetrics;
-            ref1.pointAttachment = ref.pointAttachment;
-            const innerPoints = RectifyImpl.getGlyphPoints(to1);
-            this.processTtReferenceImpl(innerPoints, st, ref1);
-            for (const z of innerPoints) {
-                st.points.push(
-                    Ot.Glyph.PointOps.applyTransform(
-                        Ot.Glyph.Point.create(z.x, z.y),
-                        ref1.transform
-                    )
+    private processContourSet(cs: Ot.Glyph.ContourSetProps, st: GlyphRectifyHandlerState) {
+        const cs1: Ot.Glyph.Contour[] = [];
+        for (const c of cs.contours) {
+            const c1: Ot.Glyph.Contour = [];
+            for (let zid = 0; zid < c.length; zid++) {
+                const z = c[zid];
+                c1[zid] = Ot.Glyph.Point.create(
+                    this.recCoord.coord(z.x),
+                    this.recCoord.coord(z.y),
+                    z.kind
                 );
             }
-            return ref1;
-        };
+            cs1.push(c1);
+        }
+
+        const g = new Ot.Glyph.ContourSet(cs1);
+        for (const c of g.contours) for (const z of c) st.points.push(z);
+        return g;
+    }
+
+    private processGeometryList(
+        items: ReadonlyArray<Ot.Glyph.Geometry>,
+        st: GlyphRectifyHandlerState
+    ) {
+        const sink: Ot.Glyph.Geometry[] = [];
+        for (const item of items) {
+            const processed = this.process(item, st);
+            if (processed) sink.push(processed);
+        }
+        if (sink.length) return new Ot.Glyph.GeometryList(sink);
+        else return null;
+    }
+
+    private processTtReference(ref: Ot.Glyph.TtReferenceProps, st: GlyphRectifyHandlerState) {
+        const to1 = this.recGlyphRef.glyphRef(ref.to);
+        if (!to1) return null;
+
+        processGlyph(this.recGlyphRef, this.recCoord, this.recPA, to1, this.globalState);
+        const ref1 = new Ot.Glyph.TtReference(to1, {
+            ...ref.transform,
+            dx: this.recCoord.coord(ref.transform.dx),
+            dy: this.recCoord.coord(ref.transform.dy)
+        });
+        ref1.roundXyToGrid = ref.roundXyToGrid;
+        ref1.useMyMetrics = ref.useMyMetrics;
+        ref1.pointAttachment = ref.pointAttachment;
+        const innerPoints = RectifyImpl.getGlyphPoints(to1);
+        this.processTtReferenceImpl(innerPoints, st, ref1);
+        for (const z of innerPoints) {
+            st.points.push(
+                Ot.Glyph.PointOps.applyTransform(Ot.Glyph.Point.create(z.x, z.y), ref1.transform)
+            );
+        }
+        return ref1;
     }
 
     private processTtReferenceImpl(
         innerPoints: Data.XY<Ot.Var.Value>[],
-        st: PointAttachmentHandlerState,
+        st: GlyphRectifyHandlerState,
         ref1: Ot.Glyph.TtReference
     ) {
         if (!ref1.pointAttachment) return;
@@ -140,37 +139,38 @@ export class OtGhRectifyGeomPointAttachmentAlg {
     }
 }
 
-class RectifyHintCoordAlg {
+class HintProcessor {
     constructor(private readonly rec: CoordRectifier) {}
 
     public process(geom: Ot.Glyph.Hint): Ot.Glyph.Hint {
         switch (geom.type) {
             case Ot.Glyph.HintType.TtInstruction:
-                return this.ttInstructions(geom);
+                return this.processTtInstructions(geom);
             case Ot.Glyph.HintType.CffHint:
-                return this.cffHint(geom);
+                return this.processCffHint(geom);
         }
     }
 
-    public ttInstructions(tt: Ot.Glyph.TtInstructionProps): Ot.Glyph.Hint {
+    private processTtInstructions(tt: Ot.Glyph.TtInstructionProps): Ot.Glyph.Hint {
         return new Ot.Glyph.TtInstruction(tt.instructions);
     }
-    public cffHint(ch: Ot.Glyph.CffHintProps): Ot.Glyph.Hint {
+
+    private processCffHint(ch: Ot.Glyph.CffHintProps): Ot.Glyph.Hint {
         const stemHMap = new Map<Ot.Glyph.CffHintStem, Ot.Glyph.CffHintStem>();
         const stemVMap = new Map<Ot.Glyph.CffHintStem, Ot.Glyph.CffHintStem>();
-        for (const s of ch.hStems) stemHMap.set(s, this.rectifyHintStem(s));
-        for (const s of ch.vStems) stemVMap.set(s, this.rectifyHintStem(s));
+        for (const s of ch.hStems) stemHMap.set(s, this.processHintStem(s));
+        for (const s of ch.vStems) stemVMap.set(s, this.processHintStem(s));
         const h1 = new Ot.Glyph.CffHint();
         h1.hStems = [...stemHMap.values()];
         h1.vStems = [...stemVMap.values()];
-        h1.hintMasks = ch.hintMasks.map(m => this.rectifyMask(m, stemHMap, stemVMap));
-        h1.counterMasks = ch.counterMasks.map(m => this.rectifyMask(m, stemHMap, stemVMap));
+        h1.hintMasks = ch.hintMasks.map(m => this.processMask(m, stemHMap, stemVMap));
+        h1.counterMasks = ch.counterMasks.map(m => this.processMask(m, stemHMap, stemVMap));
         return h1;
     }
-    private rectifyHintStem(stem: Ot.Glyph.CffHintStem) {
+    private processHintStem(stem: Ot.Glyph.CffHintStem) {
         return Ot.Glyph.CffHint.createStem(this.rec.coord(stem.start), this.rec.coord(stem.end));
     }
-    private rectifyMask(
+    private processMask(
         mask: Ot.Glyph.CffHintMask,
         stemHMap: Map<Ot.Glyph.CffHintStem, Ot.Glyph.CffHintStem>,
         stemVMap: Map<Ot.Glyph.CffHintStem, Ot.Glyph.CffHintStem>
@@ -194,16 +194,16 @@ function processGlyph(
     recCoord: CoordRectifier,
     recPA: PointAttachmentRectifier,
     glyph: Ot.Glyph,
-    gs: PointAttachmentGlobalState
+    gs: GlyphRectifyGlobalState
 ) {
-    if (gs.processed.has(glyph)) return;
+    if (gs.processedGlyphs.has(glyph)) return;
 
     if (glyph.geometry) {
-        const alg = new OtGhRectifyGeomPointAttachmentAlg(recGlyphRef, recCoord, recPA, gs);
-        glyph.geometry = alg.process(glyph.geometry)({ points: [] });
+        const alg = new GeometryProcessor(recGlyphRef, recCoord, recPA, gs);
+        glyph.geometry = alg.process(glyph.geometry, { points: [] });
     }
     if (glyph.hints) {
-        glyph.hints = new RectifyHintCoordAlg(recCoord).process(glyph.hints);
+        glyph.hints = new HintProcessor(recCoord).process(glyph.hints);
     }
     glyph.horizontal = {
         start: recCoord.coord(glyph.horizontal.start),
@@ -214,7 +214,7 @@ function processGlyph(
         end: recCoord.coord(glyph.vertical.end)
     };
 
-    gs.processed.add(glyph);
+    gs.processedGlyphs.add(glyph);
 }
 
 export function inPlaceRectifyGlyphStore<GS extends Ot.GlyphStore>(
@@ -224,7 +224,7 @@ export function inPlaceRectifyGlyphStore<GS extends Ot.GlyphStore>(
     glyphs: GS
 ) {
     const gOrd = glyphs.decideOrder();
-    const st: PointAttachmentGlobalState = { processed: new Set() };
+    const st: GlyphRectifyGlobalState = { processedGlyphs: new Set() };
 
     for (const g of gOrd) processGlyph(recGlyphRef, recCoord, recPA, g, st);
 }
