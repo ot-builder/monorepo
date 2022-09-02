c3729cc180bd1e3dd616655e6f69db94792ccf23
otbbuilder-dev <54194405+otbbuilder-dev@users.noreply.github.com>
2022-09-01 17:47:01 -0700

    Fix a typing error when compiling io-bin-layout (#107)

--- packages/io-bin-layout/src/gsub-gpos-shared/trick.ts
+++ packages/io-bin-layout/src/gsub-gpos-shared/trick.ts
@@ -2,7 +2,10 @@ import { GsubGpos } from "@ot-builder/ot-layout";
 
 import { LayoutCfg, LookupWriteTrick } from "../cfg";
 
-export function setLookupTricks<L>(table: GsubGpos.TableT<L>, cfg: LayoutCfg) {
+export function setLookupTricks<L extends GsubGpos.LookupProp>(
+    table: GsubGpos.TableT<L>,
+    cfg: LayoutCfg
+) {
     const tricks: Map<L, LookupWriteTrick> = new Map();
     if (cfg.layout.lookupWriteTricks) {
         for (const lookup of table.lookups) {
