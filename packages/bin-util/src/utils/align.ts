3b2398c5c51ba099f6040575de853c3de990154a
otbbuilder-dev <otbbuilder-dev@users.noreply.github.com>
2024-06-29 19:32:10 -0700

    Fix publish errors

--- packages/bin-util/src/utils/align.ts
+++ packages/bin-util/src/utils/align.ts
@@ -2,6 +2,7 @@ import { BufferWriter } from "../handlers/buffer-writer";
 
 export function alignBufferSize(buf: Buffer, packing: number) {
     if (packing <= 1) return buf;
+
     const bw = new BufferWriter();
     bw.bytes(buf);
     while (bw.length % packing) bw.uint8(0);
