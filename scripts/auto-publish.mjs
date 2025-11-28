157a28d79e041d66aff3e8a198068f731a7bfe44
otbbuilder-dev <otbbuilder-dev@users.noreply.github.com>
2025-11-27 20:44:32 -0800

    Bump deps and leverage npm's new publishing

--- scripts/auto-publish.mjs
+++ scripts/auto-publish.mjs
@@ -5,7 +5,6 @@ import { publish } from "./publish-procs/publish.mjs";
 
 const GitUser = "otbbuilder-dev";
 const GitEmail = "otbbuilder-dev@users.noreply.github.com";
-const NpmToken = process.env.SECRET_NPM_TOKEN;
 
 main().catch(e => {
     console.error(e);
@@ -13,12 +12,7 @@ main().catch(e => {
 });
 
 async function main() {
-    if (!NpmToken) {
-        console.error("Attempt to execute publish action on a fork. Exit.");
-        return;
-    } else {
-        const cfg = { GitUser, GitEmail, NpmToken };
-        await publish(cfg);
-        await docPublish(cfg);
-    }
+    const cfg = { GitUser, GitEmail };
+    await publish(cfg);
+    await docPublish(cfg);
 }
